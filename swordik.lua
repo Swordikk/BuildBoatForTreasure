b3841ac1a1309920c7b7b9bb9b167166813a4167
Swordikk <133685730+Swordikk@users.noreply.github.com>
2025-03-01 02:41:13 +0400

    Update swordik.lua

--- swordik.lua
+++ swordik.lua
@@ -1,1 +1,60 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Build a boat for Treasure", HidePremium = true, SaveConfig = true, ConfigFolder = "OrionTest"})
 
+local players = game:GetService("Players")
+
+-- Functions --
+local function AutoFarmMoney()
+	while _G.AutoFarmMoney == true do
+		for i = 1, 10 do
+			local localplayer = players.LocalPlayer
+			local character = localplayer.Character
+			local humanoid = character.Humanoid
+			local humanoidrootpart = character.HumanoidRootPart
+			local CaveStage = workspace.BoatStages.NormalStages["CaveStage" .. i].DarknessPart
+
+			local newpart = Instance.new("Part")
+			newpart.Parent = Workspace
+			newpart.Size = Vector3.new(5, 1, 5)
+			newpart.Anchored = true
+			newpart.CFrame = CaveStage.CFrame
+			humanoidrootpart.CFrame = CaveStage.CFrame
+			wait(2)
+			for _, v in pairs(workspace:GetChildren()) do
+    			if v:IsA("Part") then
+        			v:Destroy()
+    			end
+			end
+		end
+		Character:BreakJoints()
+		wait(5.1)
+	end
+end
+
+
+local Tab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Tab = Window:MakeTab({
+	Name = "AutoFarm",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddToggle({
+	Name = "AutoFarmMoney",
+	Default = false,
+	Callback = function(Value)
+		_G.AutoFarmMoney = Value
+		AutoFarmMoney()
+	end    
+})
+
+local Tab = Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
